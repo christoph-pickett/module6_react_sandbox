66801fbcaa0abeced5682bf19649068d245d3586
Kirby-Rose <kirbyrosemarks@gmail.com>
2025-04-02 22:27:23 +1030

    WIP: cacafa0 Merge pull request #15 from christoph-pickett/tate_feature_branch

---
+++ sandbox-react-app/src/stories/boxKirby.jsx
@@ -0,0 +1,9 @@
+const BoxKirby = ({ size = "100px", backgroundColor }) => {
+  return (
+    <div
+      style={{ height: size, width: size, backgroundColor: backgroundColor }}
+    ></div>
+  );
+};
+
+export default BoxKirby;
